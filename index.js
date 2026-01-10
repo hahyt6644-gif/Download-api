5f96af5611e9f7a287598f8883735f2b8a82cddf
hahyt6644-gif <hahyt6644@gmail.com>
2026-01-11 00:37:42 +0530

    Update and rename app.py to index.js

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+
+const app = express();
+
+app.get("/api", async (req, res) => {
+
+    try {
+
+        const url = req.query.url;
+        if(!url){
+            return res.json({status:false,msg:"URL required"});
+        }
+
+        const browser = await puppeteer.launch({
+            headless: "new",
+            args: ["--no-sandbox","--disable-setuid-sandbox"]
+        });
+
+        const page = await browser.newPage();
+
+        // wait max 10 sec
+        await page.goto(url, { waitUntil:"domcontentloaded", timeout:10000 });
+        await page.waitForTimeout(5000);
+
+        const data = await page.evaluate(()=>{
+
+            const links = [...document.querySelectorAll("a")];
+            let all = [];
+            let found = null;
+
+            links.forEach(a=>{
+                if(a.href){
+                    all.push(a.href);
+
+                    if(
+                      a.href.includes("iteraplay.tera-api") &&
+                      a.href.includes("/download?")
+                    ){
+                        found = a.href;
+                    }
+                }
+            });
+
+            return {found,all};
+        });
+
+        await browser.close();
+
+        if(data.found){
+
+            const final =
+              "https://playterabox.com/player?url=" +
+              encodeURIComponent(data.found);
+
+            return res.json({
+                status:true,
+                original:data.found,
+                player:final
+            });
+        }
+
+        return res.json({
+            status:false,
+            msg:"Download link not found",
+            all_links:data.all
+        });
+
+    } catch(err){
+
+        return res.json({
+            status:false,
+            error:err.toString()
+        });
+
+    }
+
+});
+
+app.listen(process.env.PORT || 10000, ()=>{
+    console.log("API running");
+});
